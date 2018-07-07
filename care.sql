2d8ac16bc59542140120f3a639e9bd63f6f36010
Disha S <dishas1211@gmail.com>
2018-07-07 15:41:04 +0530

    sql file containing User,Classroom,Activity,Resource allocation

---
+++ care.sql
@@ -0,0 +1,136 @@
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: careworks
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Activity`
+--
+
+DROP TABLE IF EXISTS `Activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Activity` (
+  `sid` int(11) NOT NULL,
+  `Rennovation` bit(1) DEFAULT NULL,
+  `Flooring` bit(1) DEFAULT NULL,
+  `Plastering` bit(1) DEFAULT NULL,
+  `Waterproofing` bit(1) DEFAULT NULL,
+  `Electrification` bit(1) DEFAULT NULL,
+  `Externalpainting` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Activity`
+--
+
+LOCK TABLES `Activity` WRITE;
+/*!40000 ALTER TABLE `Activity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Activity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Classroom`
+--
+
+DROP TABLE IF EXISTS `Classroom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Classroom` (
+  `Sid` int(11) NOT NULL,
+  `Numberofcomputers` int(11) DEFAULT NULL,
+  `Library` bit(1) DEFAULT NULL,
+  `Sciencelab` bit(1) DEFAULT NULL,
+  `Drinkingwater` bit(1) DEFAULT NULL,
+  `Toilet` bit(1) DEFAULT NULL,
+  `Handwash` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`Sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Classroom`
+--
+
+LOCK TABLES `Classroom` WRITE;
+/*!40000 ALTER TABLE `Classroom` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Classroom` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Resourceallocation`
+--
+
+DROP TABLE IF EXISTS `Resourceallocation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Resourceallocation` (
+  `Sid` int(11) NOT NULL,
+  `Activity` varchar(20) DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `Time` varchar(10) DEFAULT NULL,
+  `Workprogress` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`Sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Resourceallocation`
+--
+
+LOCK TABLES `Resourceallocation` WRITE;
+/*!40000 ALTER TABLE `Resourceallocation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Resourceallocation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `Uid` int(11) NOT NULL AUTO_INCREMENT,
+  `Phone` varchar(10) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `Password` varchar(20) DEFAULT NULL,
+  `AorU` varchar(5) DEFAULT NULL,
+  `Name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-07 15:31:08
